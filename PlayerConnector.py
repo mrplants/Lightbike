f92e1b0d261337daf404983dbf680d238bac0e7b
twistedTightly <mrauh1@nd.edu>
2014-05-07 18:29:56 -0400

    PlayerConnector initial commit

---
+++ PlayerConnector.py
@@ -0,0 +1,10 @@
+# PlayerConnector.py
+
+# Programming Paradigms
+# Maribeth Rauh and Sean Fitzgerald
+# 5/7/2014
+
+# This class handles the player's connection and information transfer to and from the Arena backend
+#		It initiaties a connection with Arena
+#		Updates the Arena about its turns and when it dies
+#		Recieves information from the Arena about the other player's turns and deaths
